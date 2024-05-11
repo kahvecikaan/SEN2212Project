05f4ed394689fcbe6c1c42fd6c034d667f56a593
Kaan Kahveci <kaankahveci29@outlook.com>
2024-05-11 12:00:44 +0300

    Update Lexer to work with parser and peek functionalities

--- src/com/example/expressionevaluator/lexer/Lexer.java
+++ src/com/example/expressionevaluator/lexer/Lexer.java
@@ -5,122 +5,140 @@ public class Lexer {
     private final String input;
     private int position;
     private int readPosition;
-    private int currentChar;
+    private char currentChar;
 
     public Lexer(String input) {
         this.input = input;
-        readChar();
+        readChar(); // Initialize currentChar to the first character of the input
+    }
+
+//    public Token peek() {
+//        // save the current state
+//        int tempPosition = position;
+//        int tempReadPosition = readPosition;
+//        char tempCurrentChar = currentChar;
+//
+//        Token nextToken = nextToken();
+//
+//        // restore the state
+//        position = tempPosition;
+//        readPosition = tempReadPosition;
+//        currentChar = tempCurrentChar;
+//
+//        return nextToken;
+//    }
+
+    public Token peek() {
+        // Save the current state
+        int tempPosition = position;
+        int tempReadPosition = readPosition;
+        char tempCurrentChar = currentChar;
+
+        Token nextToken = nextToken();
+
+        // Restore the state
+        position = tempPosition;
+        readPosition = tempReadPosition;
+        currentChar = tempCurrentChar;
+
+        return nextToken;
     }
 
     private void readChar() {
-        if(readPosition >= input.length()) {
-            currentChar = 0; // represents EOF
+        if (readPosition >= input.length()) {
+            currentChar = 0; // EOF, represented as null character
         } else {
             currentChar = input.charAt(readPosition);
         }
-
         position = readPosition;
         readPosition++;
     }
 
     public Token nextToken() {
+        Token token = null;
         skipWhitespace();
 
-        Token token;
-        switch(currentChar) {
-            case '+':
-                token = new Token(Token.TokenType.PLUS, "+");
-                readChar();
-                break;
-            case '-':
-                token = new Token(Token.TokenType.MINUS, "-");
-                readChar();
-                break;
-            case '/':
-                token = new Token(Token.TokenType.SLASH, "/");
-                readChar();
-                break;
-            case '*':
-                token = new Token(Token.TokenType.ASTERISK, "*");
-                readChar();
-                break;
-            case '(':
-                token = new Token(Token.TokenType.LPAREN, "(");
-                readChar();
-                break;
-            case ')':
-                token = new Token(Token.TokenType.RPAREN, ")");
-                readChar();
-                break;
-            case '=':
-                token = new Token(Token.TokenType.ASSIGN, "=");
-                readChar();
-                break;
-            case ',':
-                token = new Token(Token.TokenType.COMMA, ",");
-                readChar();
-                break;
-            case ';':
-                token = new Token(Token.TokenType.SEMICOLON, ";");
-                readChar();
-                break;
-            case 0:
-                token = new Token(Token.TokenType.EOF, "");
-                break;
-            default:
-                if(Character.isLetter(currentChar)) {
-                    String literal = readIdentifier();
-                    token = new Token(Token.lookupIdent(literal), literal);
-                }
-                else if(Character.isDigit(currentChar)) {
-                    String literal = readNumber();
-                    token = new Token(Token.TokenType.INT, literal);
-                    if(Character.isLetter(currentChar)) {
-                        return token;
-                    }
-                }
-                else {
-                    token = new Token(Token.TokenType.ILLEGAL, String.valueOf((char)currentChar));
-                    readChar(); // move past the illegal character
-                }
+        if (currentChar == 0) {
+            token = new Token(Token.TokenType.EOF, "");
+            return token;
+        }
 
-                return token;
+        if (Character.isDigit(currentChar)) {
+            token = new Token(Token.TokenType.INT, readNumber());
+        } else if (Character.isLetter(currentChar) || currentChar == '_') {
+            String ident = readIdentifier();
+            token = new Token(Token.lookupIdent(ident), ident);
+        } else {
+            switch (currentChar) {
+                case '+':
+                    token = new Token(Token.TokenType.PLUS, Character.toString(currentChar));
+                    readChar();
+                    break;
+                case '-':
+                    token = new Token(Token.TokenType.MINUS, Character.toString(currentChar));
+                    readChar();
+                    break;
+                case '*':
+                    token = new Token(Token.TokenType.ASTERISK, Character.toString(currentChar));
+                    readChar();
+                    break;
+                case '/':
+                    token = new Token(Token.TokenType.SLASH, Character.toString(currentChar));
+                    readChar();
+                    break;
+                case '(':
+                    token = new Token(Token.TokenType.LPAREN, Character.toString(currentChar));
+                    readChar();
+                    break;
+                case ')':
+                    token = new Token(Token.TokenType.RPAREN, Character.toString(currentChar));
+                    readChar();
+                    break;
+                case '=':
+                    token = new Token(Token.TokenType.ASSIGN, Character.toString(currentChar));
+                    readChar();
+                    break;
+                case ',':
+                    token = new Token(Token.TokenType.COMMA, Character.toString(currentChar));
+                    readChar();
+                    break;
+                case ';':
+                    token = new Token(Token.TokenType.SEMICOLON, Character.toString(currentChar));
+                    readChar();
+                    break;
+                default:
+                    token = new Token(Token.TokenType.ILLEGAL, Character.toString(currentChar));
+                    readChar();
+                    break;
+            }
         }
 
         return token;
     }
 
     private void skipWhitespace() {
-        while(currentChar == ' ' || currentChar == '\t' || currentChar == '\n' || currentChar == '\r') {
+        while (currentChar == ' ' || currentChar == '\t' || currentChar == '\n' || currentChar == '\r') {
             readChar();
         }
     }
 
     private String readIdentifier() {
-        int start = position;
-        while(isValidIdentifierChar(currentChar)) {
+        int startPosition = position;
+        if (!Character.isLetter(currentChar)) {
+            return Character.toString(currentChar);
+        }
+        readChar();
+        while (Character.isLetterOrDigit(currentChar) || currentChar == '_') {
             readChar();
         }
-
-        return input.substring(start, position);
+        return input.substring(startPosition, position);
     }
 
     private String readNumber() {
-        int start = position;
-
-        while(Character.isDigit(currentChar)) {
+        int startPosition = position;
+        while (Character.isDigit(currentChar)) {
             readChar();
         }
-
-        return input.substring(start, position);
-    }
-
-    private boolean isValidTokenChar(int c) {
-        return c == '+' || c == '-' || c == '*' || c == '/' || c == '(' || c == ')'
-                || c == '=' || c == ',' || c == ';' || Character.isLetter(c) || Character.isDigit(c);
-    }
-
-    private boolean isValidIdentifierChar(int c) {
-        return Character.isLetter(c) || c == '_' || (position > 0 && Character.isDigit(c));
+        return input.substring(startPosition, position);
     }
 }
